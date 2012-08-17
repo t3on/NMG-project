ed95070ba430e8a061894efb1e206e8532bf320e
Teon Brooks <teon.brooks@gmail.com>
2012-08-16 23:42:53 -0400

    CH: add blink rejection
    Add blink rejection. May go back and replace with just removing t_edf.

--- group_durations.py
+++ group_durations.py
@@ -5,8 +5,9 @@ subjects = ['R0095', 'R0224', 'R0498', 'R0499', 'R0504']
 datasets = []
 for subject in subjects:
     meg_ds = process.load_meg_events(subname = subject)
-    datasets.append(meg_ds)
+    meg_ds = process.reject_blinks(meg_ds)
+    meg_ds = datasets.append(meg_ds)
 
 group_ds = E.combine(datasets)
-group_ds.info = meg_ds.info
+group_ds.info = datasets[0].info
 group_ds.export(os.path.join(group_ds.info['expdir'], 'group', '_'.join((group_ds.info['expname'], 'group', 'durations.txt'))))
