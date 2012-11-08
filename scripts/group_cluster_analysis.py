141399d2c67c5b186ba5d88241dd788f210e2b3f
Teon Brooks <teon.brooks@gmail.com>
2012-11-07 22:41:50 -0500

    CH: rois, ADD: primes to target, and figures
    This tests newly constructed rois. It adds testing for primes to the
    test for target effects.
    Saves the clusters results as figures.

--- scripts/group_cluster_analysis.py
+++ scripts/group_cluster_analysis.py
@@ -1,17 +1,17 @@
 import eelbrain.eellab as E
-import process
-import source
+import basic.process as process
+import basic.source as source
 import copy
 import os
 import mne
 
-
+clusters_dir = os.path.join(os.path.expanduser('~'), 'Dropbox', 'Experiments', 'NMG', 'results', 'clusters')
 subjects = [('R0095', ['MEG 151']), ('R0498', ['MEG 066']), ('R0504', ['MEG 031']),
             ('R0414', []), ('R0547', ['MEG 002']), ('R0569', ['MEG 143', 'MEG 090', 'MEG 151', 'MEG 084']),
-            ('R0574', []), ('R0575', []), ('R0576', ['MEG 0143', 'MEG 046'])]
-labels = ['lh.fusiform', 'lh.medialorbitofrontal', 'lh.temporalpole']
-#'lh.parstriangularis', 'lh.superiortemporal', 'lh.lateralorbitofrontal'
-#maybe AMF
+            ('R0574', []), ('R0575', []), ('R0576', ['MEG 143'])]
+labels = ['lh.fusiform', ('lh.vmPFC', 'rh.vmPFC'), 'lh.LATL']
+
+labelnames = []
 datasets = []
 
 tstart = -0.1
@@ -21,7 +21,7 @@ reject = 3e-12
 for subject in subjects:
 
     meg_ds = process.load_meg_events(subname=subject[0], expname='NMG')
-    index = meg_ds['target'] == 'target'
+    index = meg_ds['target'].isany('prime', 'target')
 
 #create picks to remove bad channels
     if subject[1]:
@@ -35,10 +35,19 @@ for subject in subjects:
 
     #do source transformation
     for lbl in labels:
-        meg_ds = source.make_stc_epochs(meg_ds, tstart=tstart, tstop=tstop, reject=reject, label=lbl, force_fixed=True, from_file=True)
+        if type(lbl) == tuple:
+            lbl2 = lbl[1]
+            lbl = lbl[0]
+            lblname = '%s+%s' % (lbl, lbl2)
+        else:
+            lbl2 = None
+            lblname = lbl
+        labelnames.append(lblname)
+
+        meg_ds = source.make_stc_epochs(meg_ds, tstart=tstart, tstop=tstop, reject=reject, label=lbl, label2=lbl2, force_fixed=True, from_file=True)
 
     #collapsing across sources using a root-mean squared
-        meg_ds[lbl] = meg_ds[lbl].summary('source_space', func=E.statfuncs.RMS, name=lbl)
+        meg_ds[lblname] = meg_ds[lblname].summary('source_space', func=E.statfuncs.RMS, name=lblname)
 
     #delete subject epochs from memory
     del meg_ds['epochs']
@@ -49,15 +58,40 @@ for subject in subjects:
     #Append to group level datasets
     datasets.append(meg_ds)
 
-
-#Exports the source estimate ERFs 
-
 #combines the datasets for group
 group_ds = E.combine(datasets)
 
-#select only the constituent conditions
-group_ds = group_ds[group_ds['condition'].isany('control_constituent', 'first_constituent')]
+#creates indices for prime and target
+prime = group_ds['target'] == 'prime'
+target = group_ds['target'] == 'target'
 
-clusters = []for lbl in labels:
-    clusters.append(E.testnd.cluster_anova(group_ds[lbl], group_ds['condition'] * group_ds['wordtype'] * group_ds['subject']))
+#create index for only the constituent conditions
+constituent_condition = group_ds['condition'].isany('control_constituent', 'first_constituent')
+
+#creates index for only the identity conditions
+identity_condition = group_ds['condition'].isany('control_identity', 'identity')
+
+#creates index for only the prime only identity conditions
+wordtype_condition = group_ds['condition'] == 'identity'
+
+
+#creates the comparisons
+group_target_constituent = group_ds[target * constituent_condition]
+group_target_identity = group_ds[target * identity_condition]
+group_prime_wordtype = group_ds[prime * wordtype_condition]
+
+constituent_clusters = []
+identity_clusters = []
+wordtype_clusters = []for lblname in labelnames:
+    a = E.testnd.cluster_anova(lblname, 'condition' * 'wordtype' * 'subject', ds=group_target_constituent)
+    a.figure.savefig(os.path.join(clusters_dir, 'group_target_constituent_%s.png' % lbl))
+    constituent_clusters.append(a)
+
+    b = E.testnd.cluster_anova(lblname, 'condition' * 'wordtype' * 'subject', ds=group_target_identity)
+    b.figure.savefig(os.path.join(clusters_dir, 'group_target_identity_%s.png' % lbl))
+    identity_clusters.append(b)
+
+    c = E.testnd.cluster_anova(lblname, 'wordtype' * 'subject', ds=group_prime_wordtype)
+    c.figure.savefig(os.path.join(clusters_dir, 'group_prime_wordtype_%s.png' % lbl))
+    wordtype_clusters.append(c)
 
