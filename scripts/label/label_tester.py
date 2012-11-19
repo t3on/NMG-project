a97807f5621dd3df8abec3bebb2d437b38c5bf4d
Teon Brooks <teon.brooks@gmail.com>
2012-11-19 18:29:18 -0500

    DEL and MV
    Reorganization of files. Moved to respective subfolders. Some redundant
    files were deleted.

--- scripts/label/label_tester.py
+++ scripts/label/label_tester.py
@@ -2,4 +2,4 @@ import osfrom surfer import Brain
 
 rois = os.path.join(os.path.expanduser('~'), 'Dropbox', 'Experiments', 'NMG', 'talk', 'rois')subject_id = "R0095"hemi = "lh"surf = "smoothwm"brain = Brain(subject_id, hemi, surf)
 
-brain.add_label("LATL", color = 'orchid')
+brain.add_label("LPTL", color = 'orchid')brain.save_image(os.path.join(rois, "%s_LPTL.png" % subject_id))
