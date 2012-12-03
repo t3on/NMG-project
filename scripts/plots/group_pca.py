66147e1d40a61be8078f3f2cf2819862a5811043
Teon Brooks <teon.brooks@gmail.com>
2012-12-03 12:37:44 -0500

    UPDATE: close figure

--- scripts/plots/group_pca.py
+++ scripts/plots/group_pca.py
@@ -15,9 +15,11 @@ plots_dir = os.path.join(root, 'results', 'visuals', 'pca')
 e = process.NMG(root='~/data')
 
 for _ in e.iter_vars(['subject']):
-    meg_ds = e.load_events(proj = False)
+    meg_ds = e.load_events(proj=False)
     meg_ds = meg_ds[meg_ds['target'] == 'target']
-    meg_ds = E.load.fiff.add_epochs(meg_ds, tstart=-0.2, tstop=0.6, 
-                                    baseline=(-.2,0))
+    meg_ds = E.load.fiff.add_epochs(meg_ds, tstart= -0.2, tstop=0.6,
+                                    baseline=(-.2, 0))
     p = gui.pca(meg_ds, Y='MEG', nplots=(1, 1), dpi=50, figsize=(10, 6))
     p.figure.savefig(os.path.join(plots_dir, '%s_pca.pdf' % e.get('subject')))
+    p.Close(force=True)
+    del p
