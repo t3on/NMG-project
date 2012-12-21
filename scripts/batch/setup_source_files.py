ceaa141da40b3795cb9fafbb52c6bfe179c586c9
Teon Brooks <teon.brooks@gmail.com>
2012-12-21 00:04:54 -0500

    UPDATE: batch files

--- scripts/batch/setup_source_files.py
+++ scripts/batch/setup_source_files.py
@@ -5,11 +5,10 @@ Created on Oct 25, 2012
 '''
 
 import basic.process as process
- 
+
 e = process.NMG()
 
-for _ in e.iter_vars('subject'):
-    meg_ds = e.load_events()
-    #source.make_proj(meg_ds, overwrite = True)
-    #source.make_cov(meg_ds, overwrite = True)
-    e.make_fwd(overwrite=True)
+for _ in e.iter_vars(['subject']):
+#    e.make_proj(overwrite = True)
+    e.make_cov(overwrite=True)
+#    e.make_fwd(overwrite=True)
