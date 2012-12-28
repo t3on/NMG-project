88d2dbaf6ed05174b72d04c437e8b97e2519d5dd
Teon Brooks <teon.brooks@gmail.com>
2012-12-28 16:53:12 -0500

    ADD: group channel check with printout

---
+++ scripts/batch/group_channel_check.py
@@ -0,0 +1,30 @@
+'''
+Created on Nov 6, 2012
+
+@author: teon
+'''
+
+import eelbrain.eellab as E
+import basic.process as process
+import os
+
+e = process.NMG()
+channel_check = os.path.join(e.get('results'), 'logs', 'group_channel_check.txt')
+
+tstart = -.1
+tstop = .6
+reject = 3e-12
+
+tables = []
+
+with open(channel_check,'w') as FILE:
+    for _ in e.iter_vars(['subject']):
+        meg_ds = e.load_events(remove_bad_chs = False)
+        meg_ds = meg_ds[meg_ds['target'] == 'target']
+        meg_ds = E.load.fiff.add_mne_epochs(meg_ds, tstart=tstart, 
+                                            tstop=tstop, baseline=(tstart, 0), 
+                                            reject={'mag':reject}, preload=True)
+        t = E.table.frequencies(E.factor(sum(meg_ds['epochs'].drop_log, [])))
+        FILE.write(e.get('subject') + os.linesep)
+        FILE.write(str(t)+ os.linesep*3)
+    
