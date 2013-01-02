acfe47cc907c92f31bbcaf5cc0ed3c604fb414fa
Teon Brooks <teon.brooks@gmail.com>
2013-01-02 16:50:36 -0500

    ENH: create first five projs

--- scripts/plots/group_pca.py
+++ scripts/plots/group_pca.py
@@ -7,25 +7,15 @@ Created on Sep 4, 2012
 import eelbrain.eellab as E
 import os
 import basic.process as process
-from eelbrain.wxgui import MEG as gui
 
 root = os.path.join(os.path.expanduser('~'), 'Dropbox', 'Experiments', 'NMG')
 plots_dir = os.path.join(root, 'results', 'visuals', 'pca')
 
 e = process.NMG(root='~/data')
 
-#for _ in e.iter_vars(['subject']):
-#    meg_ds = e.load_events(proj=False)
-#    meg_ds = meg_ds[meg_ds['target'] == 'target']
-#    meg_ds = E.load.fiff.add_epochs(meg_ds, tstart= -0.2, tstop=0.6,
-#                                    baseline=(-.2, 0))
-#    p = gui.pca(meg_ds, Y='MEG', nplots=(1, 1), dpi=50, figsize=(10, 6))
-#    p.figure.savefig(os.path.join(plots_dir, '%s_pca.pdf' % e.get('subject')))
-#    p.Close(force=True)
-#    del p, meg_ds
 for _ in e.iter_vars(['subject']):
-    ds = e.load_events(proj = False)
+    ds = e.load_events(proj=False)
     ds = ds[ds['target'] == 'target']
     ds = E.load.fiff.add_mne_epochs(ds, tstart= -0.2, tstop=0.6,
                                     baseline=(-.2, 0))
-    e.make_proj_for_epochs(ds['epochs'], save = False, save_plot = e.get('proj_plot'))
+    e.make_proj_for_epochs(ds['epochs'], save=False, save_plot=e.get('proj_plot'))
