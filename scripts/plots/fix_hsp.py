894547600b28026eef4b3679bf5812d3bf6d0f48
Teon Brooks <teon.brooks@gmail.com>
2013-01-09 18:40:08 -0500

    ADD: plots for fix hsp and group_coreg plots

---
+++ scripts/plots/fix_hsp.py
@@ -0,0 +1,10 @@
+'''
+Created on Dec 15, 2012
+
+@author: teon
+'''
+
+import basic.process as process
+
+e = process.NMG()
+e.make_fake_mris(subject = 'R0547')
---
+++ scripts/plots/group_coreg_plots.py
@@ -0,0 +1,12 @@
+'''
+Created on Dec 11, 2012
+
+@author: teon
+'''
+import basic.process as process
+from mayavi import mlab
+
+e = process.NMG()
+
+for _ in e.iter_vars('subject'):
+    e.makeplt_coreg()
