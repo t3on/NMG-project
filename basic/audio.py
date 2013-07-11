ae73c1c7f97f8f70c32fab353f13ef81732fe470
Teon Brooks <teon.brooks@gmail.com>
2013-07-11 15:43:02 -0400

    ADD: force alignment for the audio files.

--- basic/audio.py
+++ basic/audio.py
@@ -5,44 +5,137 @@ Created on Oct 19, 2012
 '''
 
 import numpy as np
+from scipy.io.wavfile import read, write
 import os
-import shutil
+import subprocess
+import fnmatch
+import eelbrain.eellab as E
 
-def make_transcripts(audio_dir, script_dir):
-    files = os.listdir(audio_dir)
-    audio_dict = {}
-    for FILE in files:
-        if os.path.splitext(FILE)[1] == '.wav': 
-            lab = os.path.splitext(FILE)[0] 
-            lab = lab.split('_')
-            if lab[0].isdigit() or (len(lab[0]) == 0):
-                lab = lab[-1]
-            else:
-                lab = lab[0]
-#             if lab.lower() in ['practice', 'is', 'very', 'important', 'no', 'name']:
-#                 continue
-#             else:
-            lab = lab  + '.txt'
-            audio_dict[FILE] = os.path.join(script_dir, lab)
-    
-    for entry in audio_dict:
-        trans = os.path.splitext(entry)[0] + '.lab'
-        trans = os.path.join(audio_dir, trans)
-        shutil.copy(audio_dict[entry], trans) 
-            
-def del_transcripts(audio_dir):
-    files = os.listdir(audio_dir)
+
+def load_soundfiles(audio_sdir, script_dir):
+    files = os.listdir(audio_sdir)
+    exp = []
+#     times = []
+#     words = []
+#     files = []
     for FILE in files:
         if os.path.splitext(FILE)[1] == '.wav':
             lab = os.path.splitext(FILE)[0] 
             lab = lab.split('_')
-            if lab[0].isdigit() or (len(lab[0]) == 0):
-                lab = lab[-1]
+            if lab[0].isdigit():
+                word = lab[-1]
+                timestamp = lab[:-1]
+            elif len(lab[0]) == 0:
+                word = lab[-1]
+                timestamp = lab[1:-1]
             else:
-                lab = lab[0] 
-            if lab.lower() in ['practice', 'is', 'very', 'important', 'no', 'name']:
-                lab = os.path.join(audio_dir, FILE) 
-                os.remove(lab)
+                word = lab[0]
+                timestamp = lab[1:] 
+            if word.lower() in ['practice', 'is', 'very', 'important', 'no', 'name']:
+                continue
+            else:
+#                 date = timestamp[:3]
+                time = timestamp[3:]
+                time = map(int, time)
+                time = (time[0]*60*60   # hours to secs
+                        + time[1]*60    # minutes to secs
+                        + time[2])      # secs
+                
+                trans_file = os.path.join(script_dir, word + '.txt')
+                transcript = open(trans_file, 'r').readlines()
+                word1, word2 = [a.strip() for a in transcript]
+                
+                FILE = os.path.join(audio_sdir, FILE)
+                
+                exp.append([time, word, word1, word2, FILE])
+    exp.sort()
+
+    times, words, word1s, word2s, files = zip(*exp)
+    ds = E.dataset()
+    ds['time'] = E.var(times)
+    ds['word'] = E.factor(words)
+    ds['file'] = E.factor(files)
+    ds['word1'] = E.factor(word1s)
+    ds['word2'] = E.factor(word2s)
+    p = -1
+    block = []
+    for i in xrange(ds.n_cases):
+        if i%240 == 0:
+            p += 1
+        block.append(p)
+    ds['block'] = E.var(block)
+        
+    
+    return ds
+
+
+def make_transcripts(audio_sdir, script_dir, data_sdir, name):
+    ds = load_soundfiles(audio_sdir, script_dir)
+    for blocknum in np.unique(ds['block']):
+        filename = '_'.join((name, 'block-' + str(blocknum), 'concatenated.txt'))
+        filename = os.path.join(data_sdir, filename)
+        idx = ds['block'] == blocknum
+        ds[idx, ('word1', 'word2')].save_txt(path = filename, 
+                                             delim = os.linesep, 
+                                             header = False)
+
+
+def cat_soundfiles(audio_sdir, script_dir, data_sdir, name):
+    ds = load_soundfiles(audio_sdir, script_dir)
+    for blocknum in np.unique(ds['block']):
+        filename = '_'.join((name, 'block-' + str(blocknum), 'concatenated.wav'))
+        filename = os.path.join(data_sdir, filename)
+        idx = ds['block'] == blocknum
+        cat_list = ds['file'][idx]
+        cat_soundfile = []
+        rates = []
+        for FILE in cat_list:
+            rate, data = read(FILE)
+            cat_soundfile.append(data)
+            rates.append(rate)
+        if all(rate == rates[0] for rate in rates):
+            if cat_soundfile[0].ndim == 1:
+                cat_soundfile = np.hstack(cat_soundfile)
+            elif cat_soundfile[0].ndim == 2:
+                cat_soundfile = np.vstack(cat_soundfile)
+            else:
+                raise TypeError('Not a proper soundfile!')
+            write(filename, rate, cat_soundfile)
+
+
+def force_align(data_sdir):
+
+    os.environ['PATH'] = ':'.join([os.getenv('PATH'), '/Applications/p2fa', 
+                                   '/Applications/htk'])
+    
+    import tempfile
+    tmp_dir = tempfile.mkdtemp()
+    files = os.listdir(data_sdir)
+    files = fnmatch.filter(files, '*.wav')
+
+    for FILE in files:
+        title, _ = os.path.splitext(FILE)
+        transcript = title + '.txt'
+        textgrid = title + '.TextGrid'
+        
+        cmd = ['/opt/local/bin/sox', os.path.join(data_sdir, FILE), 
+               '-r 11025',  '-c 1', 
+               os.path.join(tmp_dir, 'temp.wav')]        
+        cwd = '/Applications/p2fa/'
+        sp = subprocess.call(cmd, cwd = cwd)
+        
+        cmd = ['python', 'align.py', os.path.join(tmp_dir, 'temp.wav'), 
+               os.path.join(data_sdir, transcript),
+               os.path.join(data_sdir, textgrid)]
+
+        sp = subprocess.Popen(cmd, cwd=cwd,
+                              stdout=subprocess.PIPE, 
+                              stderr=subprocess.PIPE)
+        stdout, stderr = sp.communicate()
+
+        if stderr:
+            print '\n> ERROR:'
+            print '%s\n%s' %(stderr, stdout)
     
 
 class Sound:
